860df65fb48a5932c46245cf45bc37b127533f03
Andrzej Sliwa <andrzej.sliwa@i-tool.eu>
2013-04-08 06:19:57 +0200

    fix location of hist.config

--- customs/erlang.bash
+++ customs/erlang.bash
@@ -1,6 +1,6 @@
 alias r='rebar'
 alias rh='rebar -c'
-alias erl='erl -config erl_hist.config -name term@127.0.0.1 -pa $PWD/ebin -pa $PWD/apps/*/ebin -pa $PWD/deps/*/ebin'
+alias erl='erl -config ~/erl_hist.config -name term@127.0.0.1 -pa $PWD/ebin -pa $PWD/apps/*/ebin -pa $PWD/deps/*/ebin'
 
 function erlang {
    source ~/erlangs/$1/activate
