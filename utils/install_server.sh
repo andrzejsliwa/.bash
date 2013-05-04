35381da7545a21b7365e4e1b4c0a3a7e613f673c
Andrzej Sliwa <andrzej.sliwa@i-tool.eu>
2013-05-04 22:32:01 +0200

    hotfix

--- utils/install_server.sh
+++ utils/install_server.sh
@@ -6,4 +6,5 @@ then
 else
   git clone git@github.com:andrzejsliwa/.bash.git ~/.andrzej-bash
 fi
-grep "alias abash" ~/.bashrc >/dev/null       || echo 'alias abash="source ~/.andrzej-bash/init.bash"'
+grep "alias abash" ~/.bashrc >/dev/null       || echo 'alias abash="source ~/.andrzej-bash/init.bash"' >> ~/.bashrc
+source ~/.bashrc
