3861cb354d02ace2fc72a4dc26970aff85a944f7
Andrzej Sliwa <andrzej.sliwa@i-tool.eu>
2013-05-04 22:37:08 +0200

    prepare server version

---
+++ init-server.bash
@@ -0,0 +1,5 @@
+export EDITOR=vim
+source ~/.andrzej-bash/logging.bash
+verbosity=$lvl_info
+
+source ~/.andrzej-bash/includes.bash
--- utils/install_server.sh
+++ utils/install_server.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-if [ -d $HOME/.bash ]
+if [ -d $HOME/.andrzej-bash ]
 then
-  cd ~/.bash ; git pull
+  cd ~/.andrzej-bash ; git pull
 else
   git clone git@github.com:andrzejsliwa/.bash.git ~/.andrzej-bash
 fi
-grep "alias abash" ~/.bashrc >/dev/null       || echo 'alias abash="source ~/.andrzej-bash/init.bash"' >> ~/.bashrc
+grep "alias abash" ~/.bashrc >/dev/null       || echo 'alias abash="source ~/.andrzej-bash/init-server.bash"' >> ~/.bashrc
 source ~/.bashrc
